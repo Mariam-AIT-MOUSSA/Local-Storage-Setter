55d91696a44bdcc5a1a43f9c0cd0805d7a97041a
Mariam AIT MOUSSA <mariam15aitmoussa@gmail.com>
2024-10-22 09:55:20 +0100

    quickfix

--- src/components/readingFromLocalStorage.jsx
+++ src/components/readingFromLocalStorage.jsx
@@ -2,16 +2,15 @@ import useLocalStorage from "../utilities/useLocalStorage";
 
 export default function ReadingFromLocalStorage() {
 
-  //For reading email ... 
-  const emailKey = "email";
 
-  const [storedValue] = useLocalStorage(emailKey, '');
+  const [email] = useLocalStorage('email', '');
+  const [name] = useLocalStorage('name', '');
 
   return (
     <header className="bg-blue-600 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">My App</h1>
-        <p className="text-white text-sm">Email: {storedValue ? storedValue : 'guest@example.com'}</p>
+        <h1 className="text-white text-2xl font-bold">Hello {name ? name : 'Guest'}</h1>
+        <p className="text-white text-sm"> {email ? email : 'guest@example.com'}</p>
       </div>
     </header>
   )
--- src/components/settingInLocalStorage.jsx
+++ src/components/settingInLocalStorage.jsx
@@ -3,9 +3,11 @@ import useLocalStorage from "../utilities/useLocalStorage";
 
 export default function SettingInLocalStorage() {
 
-  const [storedValue, setStoredValue] = useLocalStorage('email', '');
+  const [storedEmail, setStoredEmail] = useLocalStorage('email', '');
+  const [storedName, setStoredName] = useLocalStorage('name', '');
 
-  const [value, setValue] = useState(storedValue);
+  const [email, setEmail] = useState(storedEmail);
+  const [name, setName] = useState(storedName);
 
   return (
     <div className="App flex justify-center min-h-screen p-8 bg-gray-100">
@@ -14,18 +16,28 @@ export default function SettingInLocalStorage() {
           <input
             type="text"
             placeholder='enter your email'
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder='enter your name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
 
         <div>
           <button
-            onClick={() => setStoredValue(value)}
+            onClick={() => {setStoredEmail(email); setStoredName(name)}}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
-            Set email in LocalStorage
+            Set in local storage
           </button>
         </div>
       </div>
